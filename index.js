5d0549b7fbe340541c553373229fd8effed18054
lokesh yadav <lokesh_rbs@yahoo.com>
2018-07-03 16:58:41 +0530

    Assignment 6.3

---
+++ index.js
@@ -0,0 +1,20 @@
+// Javascript file for assignment 6.3
+
+// variable declaration for celsius
+var temperatureInCelsius = 40;
+
+// convert into fahrenheit
+var celsiusToFahrenheit = (temperatureInCelsius *9)/5 + 32;
+
+// log values to console
+console.log('Temprature of '+ temperatureInCelsius +' celsius, will be ' + celsiusToFahrenheit +' fahrenheit');
+
+// variable declaration fro fahrenheit
+var temperatureInFahrenheit = 104;
+
+// convert into celsius
+var fahrenheitToCelsius = ((temperatureInFahrenheit -32)*5)/9;
+
+
+//// log values to console
+console.log('Temprature of '+ temperatureInFahrenheit +' fahrenheit, will be ' + fahrenheitToCelsius +' celsius');
